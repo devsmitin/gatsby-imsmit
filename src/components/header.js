4c688a0eff20a462398ba547bf397e934a9937d6
Smit Modi <smit.modi@multidots.com>
2019-02-06 15:28:36 +0530

    swapped theme

---
+++ src/components/header.js
@@ -0,0 +1,41 @@
+import { Link } from "gatsby";
+import PropTypes from "prop-types";
+import React from "react";
+
+const Header = ({ siteTitle }) => (
+	<header
+		style={{
+			background: `#cc0000`
+		}}
+	>
+		<div
+			style={{
+				margin: `0 auto`,
+				maxWidth: 640,
+				padding: `1rem`
+			}}
+		>
+			<h1 style={{ margin: 0 }}>
+				<Link
+					to="/"
+					style={{
+						color: `white`,
+						textDecoration: `none`
+					}}
+				>
+					{siteTitle}
+				</Link>
+			</h1>
+		</div>
+	</header>
+);
+
+Header.propTypes = {
+	siteTitle: PropTypes.string
+};
+
+Header.defaultProps = {
+	siteTitle: ``
+};
+
+export default Header;
---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "gatsby";
+
+import Layout from "../components/layout";
+// import Image from "../components/image"
+import SEO from "../components/seo";
+
+const IndexPage = () => (
+	<Layout>
+		<SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+		<h1>Hi people</h1>
+		<p>Welcome to my Gatsby site.</p>
+		<Link to="/page-2/">Checkout page 2</Link>
+	</Layout>
+);
+
+export default IndexPage;
